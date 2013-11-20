06517ea1f0ee637e26987fd1c20794f6d0a1b6b6
Binh Vu <vuducbinh2001@yahoo.com>
2013-11-20 23:00:25 +0100

    compress & decompress functions

--- haskell/src/P2P/Compression.hs
+++ haskell/src/P2P/Compression.hs
@@ -1,9 +1,22 @@
 module P2P.Compression where
 
 import qualified Data.ByteString as BS
+import qualified Codec.Compression.LZ4 as LZ
 
 compress :: BS.ByteString -> (BS.ByteString, Bool)
-compress msg = (msg, True)
+compress msg = case LZ.compress msg of
+					Just result -> (result, True)
+					Nothing		-> (msg, False)
 
 decompress :: BS.ByteString -> BS.ByteString
-decompress msg = undefined
+decompress msg = case LZ.decompress msg of
+					Just result -> result
+					Nothing		-> msg
+
+{-
+test = do
+    contents <- BS.readFile "foo.txt"
+    let (result, flag) = compress contents
+    print $ BS.length result
+    print $ BS.length (decompress result)
+-}
