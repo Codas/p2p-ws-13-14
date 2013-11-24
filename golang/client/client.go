e631a83bbccdf72590c766d99637830c921103e3
Reimond Retz <reimond@web.de>
2013-11-24 06:51:00 +0100

    client implementation
    does not yet play nice with haskell server (not sure if client or server is at fault)

--- golang/client/client.go
+++ golang/client/client.go
@@ -2,45 +2,96 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
-	"io"
 	"net"
 	"os"
 	"strconv"
+	"strings"
+
+	"../client/protocol"
 )
 
 var (
 	ip   = flag.String("ip", "localhost", "Server Ip Address")
 	port = flag.Int("port", 1337, "Server port")
-	msg  = flag.String("msg", "", "Message to send. if empty, input stream is being send")
+	//msg  = flag.String("msg", "", "Message to send. if empty, input stream is being send")
 )
+
+func parseStdIO(pc *protocol.Connection) {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		text := scanner.Text()
+		if idx := strings.Index(text, " "); idx != -1 {
+			command := text[:idx]
+			text = text[idx+1:]
+			switch command {
+			case "j":
+				pc.WriteJoinTopics(strings.Split(text, ","))
+			case "l":
+				pc.WritePartTopics(strings.Split(text, ","))
+			case "b":
+				pc.WriteBroadCast(text)
+			default:
+				topics := strings.Split(command, ",")
+				pc.WriteMessage(topics, text)
+			}
+		} else {
+			switch text {
+			case "t":
+				pc.WriteAskTopics()
+			case "q":
+				pc.Close()
+			}
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading stdio:", err)
+	}
+}
 
 func main() {
 	flag.Parse()
 
 	c, err := net.Dial("tcp", *ip+":"+strconv.Itoa(*port))
 	if err != nil {
-		fmt.Errorf("Dialing Error: %s\n", err)
+		fmt.Fprintln(os.Stderr, "Dialing Error: ", err)
 		return
 	}
 	defer c.Close()
 
-	if *msg != "" {
-		n, err := c.Write([]byte(*msg))
+	fmt.Println("COMMANDS")
+	fmt.Println(" - q (QUIT)")
+	fmt.Println(" - t (ask for topics list)")
+	fmt.Println(" - j <channel> (join <channel>)")
+	fmt.Println(" - l <channel> (leave <channel>)")
+	fmt.Println(" - b <message> (broadcast <message>)")
+	fmt.Println(" - <channel> <message> (send <message> to <channel>)")
+
+	pc := protocol.NewConnection(c)
+
+	go parseStdIO(pc)
+
+	for {
+		p, err := pc.ReadPackage()
 		if err != nil {
-			fmt.Errorf("Writing Error: %s\n", err)
+			fmt.Fprintln(os.Stderr, "Error reading package: ", err)
 			return
 		}
-		if n != len(*msg) {
-			fmt.Errorf("Writing size Mismatch: %d != %d \n", n, len(*msg))
-			return
-		}
-	} else {
-		_, err = io.Copy(c, os.Stdin)
-		if err != nil {
-			fmt.Errorf("Copy Error: %s\n", err)
-			return
+		switch p.Flags {
+		//case protocol.FlagJoin:
+		//case protocol.FlagPart:
+		//case protocol.FlagTopicAsk:
+		case protocol.FlagTopicReceive:
+			fmt.Printf("[TOPICS] %s\n", p.Topics)
+		case protocol.FlagMessage:
+			fmt.Printf("%v %s\n", p.Topics, p.Message)
+		case protocol.FlagBinary:
+			fmt.Printf("(b)%v %s\n", p.Topics, p.Message)
+		case protocol.FlagBroadCast:
+			fmt.Printf("[BROADCAST] %s\n", p.Message)
 		}
 	}
 }
---
+++ golang/client/protocol/protocol.go
@@ -0,0 +1,234 @@
+package protocol
+
+import (
+	"bufio"
+	"io"
+)
+
+type Connection struct {
+	r *bufio.Reader
+	w *bufio.Writer
+	c io.Closer
+}
+
+func NewConnection(rwc io.ReadWriteCloser) *Connection {
+	return &Connection{
+		r: bufio.NewReader(rwc),
+		w: bufio.NewWriter(rwc),
+		c: rwc,
+	}
+}
+
+func numBytes(length uint64) int {
+	// mathematisch bestimmen:
+	// numbytes := int(math.Ceil((math.Floor(math.Log2(float64(length))) - 4) / 8)) + 1
+	switch {
+	case length < 1<<5:
+		return 1
+	case length < 1<<13:
+		return 2
+	case length < 1<<21:
+		return 3
+	case length < 1<<29:
+		return 4
+	case length < 1<<37:
+		return 5
+	case length < 1<<45:
+		return 6
+	case length < 1<<53:
+		return 7
+	default:
+		return 8
+	}
+}
+
+func (c *Connection) encodeLength(length uint64) error {
+	numbytes := numBytes(length)
+	for i := 0; i < numbytes; i++ {
+		b := byte((length >> (8 * uint(numbytes-i-1))) & 0xFF)
+		if i == 0 {
+			b |= byte((numbytes - 1) << 5)
+		}
+		if err := c.w.WriteByte(b); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (c *Connection) decodeLength() (length uint64, err error) {
+	// read first byte
+	b, err := c.r.ReadByte()
+	if err != nil {
+		return 0, err
+	}
+	numbytes := int((b&(7<<5))>>5) + 1
+
+	// read extra bytes
+	bytes := make([]byte, numbytes)
+	// clear out our three length bits
+	bytes[0] = b & 31
+	_, err = c.r.Read(bytes[1:])
+	if err != nil {
+		return 0, err
+	}
+
+	// convert to uint64
+	for i := range bytes {
+		length |= (uint64(bytes[i]) << uint(8*(numbytes-i-1)))
+	}
+	return
+}
+
+func (c *Connection) WriteJoinTopics(topics []string) error {
+	if err := c.w.WriteByte(FlagJoin << 3); err != nil {
+		return err
+	}
+	if err := c.writeTopics(topics); err != nil {
+		return err
+	}
+	return c.w.Flush()
+}
+
+func (c *Connection) WritePartTopics(topics []string) error {
+	if err := c.w.WriteByte(FlagPart << 3); err != nil {
+		return err
+	}
+	if err := c.writeTopics(topics); err != nil {
+		return err
+	}
+	return c.w.Flush()
+}
+
+func (c *Connection) WriteBroadCast(message string) error {
+	msg, compressed := compressMessage([]byte(message))
+	var code byte = FlagBroadCast << 3
+	if compressed {
+		code |= MaskZip
+	}
+	if err := c.w.WriteByte(code); err != nil {
+		return err
+	}
+	if err := c.writeMessage(msg); err != nil {
+		return err
+	}
+	return c.w.Flush()
+}
+
+func (c *Connection) WriteMessage(topics []string, message string) error {
+	msg, compressed := compressMessage([]byte(message))
+	var code byte = FlagMessage << 3
+	if compressed {
+		code |= MaskZip
+	}
+	if err := c.w.WriteByte(code); err != nil {
+		return err
+	}
+	if err := c.writeTopics(topics); err != nil {
+		return err
+	}
+	if err := c.writeMessage(msg); err != nil {
+		return err
+	}
+	return c.w.Flush()
+}
+
+func (c *Connection) WriteAskTopics() error {
+	if err := c.w.WriteByte(FlagTopicAsk << 3); err != nil {
+		return err
+	}
+	return c.w.Flush()
+}
+
+func (c *Connection) writeTopics(topics []string) error {
+	length := uint64(len(topics) - 1)
+	for _, t := range topics {
+		length += uint64(len(t))
+	}
+	if err := c.encodeLength(length); err != nil {
+		return err
+	}
+	for i, t := range topics {
+		if _, err := c.w.WriteString(t); err != nil {
+			return err
+		}
+		if i < len(topics)-1 {
+			if err := c.w.WriteByte(0); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func (c *Connection) writeMessage(msg []byte) error {
+	length := len(msg)
+	if err := c.encodeLength(uint64(length)); err != nil {
+		return err
+	}
+	if _, err := c.w.Write(msg); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *Connection) ReadPackage() (p *Package, err error) {
+	b, err := c.r.ReadByte()
+	if err != nil {
+		return nil, err
+	}
+
+	p = &Package{}
+	p.Flags = Flags((b & MaskAction) >> 3)
+	compressed := b&MaskZip != 0
+	if p.hasTopics() {
+		if p.Topics, err = c.readTopics(); err != nil {
+			return nil, err
+		}
+	}
+	if p.hasMessage() {
+		if p.Message, err = c.readMessage(compressed); err != nil {
+			return nil, err
+		}
+	}
+	return
+}
+
+func (c *Connection) readTopics() (topics []string, err error) {
+	length, err := c.decodeLength()
+	if err != nil {
+		return nil, err
+	}
+	data := make([]byte, length)
+	if _, err = c.r.Read(data); err != nil {
+		return
+	}
+	laststart := 0
+	for i, b := range data {
+		if b == 0 {
+			topics = append(topics, string(data[laststart:i]))
+			laststart = i + 1
+		}
+	}
+	topics = append(topics, string(data[laststart:len(data)]))
+	return
+}
+
+func (c *Connection) readMessage(compressed bool) (msg []byte, err error) {
+	length, err := c.decodeLength()
+	if err != nil {
+		return nil, err
+	}
+	msg = make([]byte, length)
+	if _, err = c.r.Read(msg); err != nil {
+		return
+	}
+	if compressed {
+		msg = decompressMessage(msg)
+	}
+	return
+}
+
+func (c *Connection) Close() error {
+	return c.c.Close()
+}
