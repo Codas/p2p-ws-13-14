91ad2ba9d35b7062340b6e6d3a5169bfb02cb707
Arne Link <mail@arne-link.de>
2013-11-24 15:32:52 +0100

    Flush wirkt wunder.. Außerdem problem mit broadcasts behoben ...
    clients ohne topic wurden erst nicht registriert

--- haskell/src/Server.hs
+++ haskell/src/Server.hs
@@ -32,7 +32,7 @@ import qualified P2P.Protocol         as P
 data Opts = Opts
   { opPort    :: String
   , opAddress :: Net.HostPreference
-  , relay     :: Maybe String
+  , opRelay   :: Maybe String
   , opConsole :: Bool
   , opGui     :: Bool }
   deriving Show
@@ -91,39 +91,54 @@ opts = info (serverOpts <**> helper)
 main :: IO ()
 main = Net.withSocketsDo $ do
     options <- execParser opts
+    (netEvent, pEvt) <- Evt.getEventBus
 
-    (netEvent, pushEvent) <- Evt.getEventBus
     initUIs netEvent options
 
-  -- listen for incomig connections.
-    let address = opAddress options
+    startServer options netEvent pEvt
+    -- _ <- forkIO $ connectToRelay options netEvent pEvt
+
+    return ()
+
+startServer :: Opts -> Evt.NetEventGetter -> (Evt.NetEvent -> IO b) -> IO r
+startServer options netEvent pEvt = listen $ \(lSock, lAddr) -> do
+    let server     = Evt.Client lAddr Nothing
+        serverInfo = Evt.MessageInfo 0 Set.empty
+    topicB  <- getTopicBehavior  netEvent
+    clientB <- getClientBehavior netEvent
+    _ <- pEvt $ Evt.NetEvent Evt.Ready server serverInfo
+    forever . Net.acceptFork lSock $ \(rHandle, rAddr) -> do
+        let client = Evt.Client rAddr (Just rHandle)
+        handleClient rHandle client topicB clientB pEvt
+  where listen = Net.listen address port
+        address = opAddress options
         port    = opPort options
-    -- TODO: server api verwenden?
-    Net.listen address port $ \(listenSocket, listenAddr) -> do
-        let server = Evt.Client listenAddr Nothing
-            serverInfo   = Evt.MessageInfo 0 Set.empty
-        pushEvent $ Evt.NetEvent Evt.Ready server serverInfo
-        topicB <- getClientBehavior netEvent
-        forever . Net.acceptFork listenSocket $ \(connHandle, remoteAddr) -> do
-            let client = Evt.Client remoteAddr (Just connHandle)
-            handleClient connHandle client topicB pushEvent
+
 
 -- Read content from socket handle, display it and notify system of events
-handleClient :: Handle -> Evt.Client -> Behavior (Set TopicClients) -> (Evt.NetEvent -> IO b) -> IO ()
-handleClient handle client topicB pushEvent = do
+handleClient :: Handle -> Evt.Client -> Behavior (Set TopicClients)
+             -> Behavior (Set ClientCon) -> (Evt.NetEvent -> IO b) -> IO ()
+handleClient handle client topicB clientB pushEvent = do
     let clientInfo = Evt.MessageInfo 0 Set.empty
     _ <- pushEvent $ Evt.NetEvent Evt.Connected client clientInfo
     clientStream <- LS.hGetContents handle
-    let eventTuple = (topicB, pushEvent, client)
+    let eventTuple = (topicB, clientB, pushEvent, client)
     handleProtocol eventTuple clientStream
     _ <- pushEvent $ Evt.NetEvent Evt.Disconnected client clientInfo
     return ()
+
+connectToRelay :: Opts -> Evt.NetEventGetter -> (Evt.NetEvent -> IO b) -> IO ()
+connectToRelay options netEvent pEvt = case opRelay options of
+    Nothing    -> return ()
+    Just rAddr -> undefined
+
 
 -----------------------------------
 -- Protocol and client handling. --
 -----------------------------------
 
-type EventTuple b = (Behavior (Set TopicClients), Evt.NetEvent -> IO b, Evt.Client)
+type EventTuple b = (Behavior (Set TopicClients), Behavior (Set ClientCon)
+                    , Evt.NetEvent -> IO b, Evt.Client)
 
 handleProtocol :: EventTuple b -> LS.ByteString -> IO ()
 handleProtocol evtTuple bs
@@ -140,7 +155,7 @@ handleProtocol evtTuple bs
 
 -- client handling
 handleJoin :: EventTuple b -> M.NetMessage -> IO ()
-handleJoin (topicB, pEvt, client) nMsg = case M.topics nMsg of
+handleJoin (topicB, _, pEvt, client) nMsg = case M.topics nMsg of
     Nothing -> return ()
     Just ts -> do
         currentTs <- currentValue topicB
@@ -151,7 +166,7 @@ handleJoin (topicB, pEvt, client) nMsg = case M.topics nMsg of
         return ()
 
 handlePart :: EventTuple b -> M.NetMessage -> IO ()
-handlePart (topicB, pEvt, client) nMsg = case M.topics nMsg of
+handlePart (topicB, _, pEvt, client) nMsg = case M.topics nMsg of
     Nothing -> return ()
     Just ts -> do
         currentTs <- currentValue topicB
@@ -163,14 +178,15 @@ handlePart (topicB, pEvt, client) nMsg = case M.topics nMsg of
         return ()
 
 handleMessage :: EventTuple b -> M.NetMessage -> IO ()
-handleMessage (topicB, pEvt, client) nMsg = case nMsg of
+handleMessage (topicB, _, pEvt, client) nMsg = case nMsg of
     (M.NetMessage _ (Just ts) (Just msg)) -> do
         currentTs <- currentValue topicB
         let i       = Evt.MessageInfo (Text.length msg) ts
             binMsg  = M.messageToByteString nMsg
             subTs   = Set.filter (\tc -> Set.member (_topic tc) ts) currentTs
         _ <- pEvt $ Evt.NetEvent Evt.Message client i
-        forClientHandles subTs client $ \h -> BS.hPut h binMsg
+
+        forClientHandles subTs client $ \h -> BS.hPut h binMsg >> hFlush h
 
         -- TODO: Debugging only, remove when done...
         -- outputs current message to the console
@@ -179,14 +195,17 @@ handleMessage (topicB, pEvt, client) nMsg = case nMsg of
     _ -> return ()
 
 handleBroadcast :: EventTuple b -> M.NetMessage -> IO ()
-handleBroadcast (topicB, pEvt, client) nMsg = case M.message nMsg of
+handleBroadcast (_, clientB, pEvt, client) nMsg = case M.message nMsg of
     Nothing  -> return ()
     Just msg -> do
-        currentTs <- currentValue topicB
+        currentCs <- currentValue clientB
         let i          = Evt.MessageInfo (Text.length msg) Set.empty
             binMsg     = M.messageToByteString nMsg
+            cs = mapMaybe (Evt.clientHandle . _client ) $ Set.toList currentCs
         _ <- pEvt $ Evt.NetEvent Evt.Broadcast client i
-        forClientHandles currentTs client $ \h -> BS.hPut h binMsg
+        forM_ cs $ \h -> BS.hPut h binMsg >> hFlush h
+        print cs
+        print binMsg
 
         -- TODO: Debugging only, remove when done...
         -- outputs current message to the console
@@ -200,7 +219,7 @@ clientsToHandles cs c = mapMaybe Evt.clientHandle noCBClient
 -- concurrently execute function for all client handles in the topicClients set.
 -- Exclude single client from the set.
 forClientHandles :: Set TopicClients -> Evt.Client -> (Handle -> IO ()) -> IO ()
-forClientHandles tcs c fn = forM_ handles $ forkIO . fn
+forClientHandles tcs c fn = (forM_ handles $ forkIO . fn) >> print handles
   where cs = Set.foldr (Set.union . _clients) Set.empty tcs
         handles = clientsToHandles cs c
 
@@ -236,8 +255,11 @@ instance Ord TopicClients where
 clients :: Lens' TopicClients (Set Evt.Client)
 clients = lens _clients (\topicClient t -> topicClient { _clients = t })
 
-getClientBehavior :: Evt.NetEventGetter -> IO (Behavior (Set TopicClients))
-getClientBehavior evt = accumB Set.empty ( accTopicClients <$> evt Evt.AnyEvent)
+getClientBehavior :: Evt.NetEventGetter -> IO (Behavior (Set ClientCon))
+getClientBehavior evt = accumB Set.empty ( accClientCon <$> evt Evt.AnyEvent)
+
+getTopicBehavior :: Evt.NetEventGetter -> IO (Behavior (Set TopicClients))
+getTopicBehavior evt = accumB Set.empty ( accTopicClients <$> evt Evt.AnyEvent)
 
 accClientCon :: Evt.NetEvent -> Set ClientCon -> Set ClientCon
 accClientCon (Evt.NetEvent eType c i) cns =
