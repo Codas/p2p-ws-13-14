03a5994a9374b683ecea8275d9d52cacf1fa2bfb
Arne Link <mail@arne-link.de>
2013-11-24 10:01:02 +0100

    Minor cleanup
    topics, message etc variablen haben die bestehenden funktionen messages,
      topics etc überschattet. Habe die variablen umbenannt

--- haskell/src/P2P/Messages.hs
+++ haskell/src/P2P/Messages.hs
@@ -14,9 +14,9 @@ data NetMessage = NetMessage
                   deriving (Show)
 
 messageToByteString :: NetMessage -> BS.ByteString
-messageToByteString (NetMessage cmd topics message) = result
-        where topicBS                  = unparseTopics topics
-              messageBS                = unparseMessage message
+messageToByteString (NetMessage cmd ts msg) = result
+        where topicBS                  = unparseTopics ts
+              messageBS                = unparseMessage msg
               body                     = topicBS `BS.append` messageBS
               zipping                  = BS.length body > 1000
               cmdBS                    = unparseCommand cmd zipping
@@ -27,23 +27,25 @@ messageToByteString (NetMessage cmd topics message) = result
 
 
 byteStringToMessage :: LS.ByteString -> (NetMessage, LS.ByteString)
-byteStringToMessage ls = if zipped then handleZippedMessage (LS.tail ls) mCmd else handleNormalMessage (LS.tail ls) mCmd
+byteStringToMessage ls = if zipped then zippedMessage else normalMessage
         where mCmd@(Just cmd)   = parseCommand $ LS.head ls
               (Flags zipped)    = parseFlags $ LS.head ls
+              zippedMessage     = handleZippedMessage (LS.tail ls) mCmd
+              normalMessage     = handleNormalMessage (LS.tail ls) mCmd
 
 handleNormalMessage :: LS.ByteString -> Maybe Command -> (NetMessage, LS.ByteString)
-handleNormalMessage ls command = (NetMessage cmd topics message, messageRest)
-        where (Just cmd)              = command
-              (topics, topicRest)     = extractTopics ls command
-              (message, messageRest)  = extractMessage topicRest command
+handleNormalMessage ls mCmd = (NetMessage cmd ts msg, messageRest)
+        where (Just cmd)         = mCmd
+              (ts, topicRest)    = extractTopics ls mCmd
+              (msg, messageRest) = extractMessage topicRest mCmd
 
 handleZippedMessage :: LS.ByteString -> Maybe Command -> (NetMessage, LS.ByteString)
-handleZippedMessage ls command = (NetMessage cmd topics message, rest)
-        where (Just cmd)                            = command
+handleZippedMessage ls mCmd = (NetMessage cmd ts msg, rest)
+        where (Just cmd)                            = mCmd
               Just (lengthLength, compressedLength) = parseLength ls
               (decompressedBody, rest)              = decompressStream (LS.drop lengthLength ls) compressedLength
-              (topics, topicRest)                   = extractTopics decompressedBody command
-              (message, messageRest)                = extractMessage topicRest command
+              (ts, topicRest)                       = extractTopics decompressedBody mCmd
+              (msg, messageRest)                    = extractMessage topicRest mCmd
 
 
 containsTopics :: Maybe Command -> Bool
@@ -67,12 +69,12 @@ containsMessage (Just cmd) = case cmd of
 
 extractTopics :: LS.ByteString -> Maybe Command -> (Maybe Topics, LS.ByteString)
 extractTopics ls Nothing = (Nothing, ls)
-extractTopics ls cmd     = if containsTopics cmd then (Just topics, rest) else (Nothing, ls)
-       where Just tTuple    =  parseTopics ls
-             (topics, rest) = tTuple
+extractTopics ls cmd     = if containsTopics cmd then (Just ts, rest) else (Nothing, ls)
+       where Just tTuple =  parseTopics ls
+             (ts, rest)  = tTuple
 
 extractMessage :: LS.ByteString -> Maybe Command -> (Maybe Message, LS.ByteString)
 extractMessage ls Nothing = (Nothing, ls)
-extractMessage ls cmd     = if containsMessage cmd then (Just message, rest) else (Nothing, ls)
-       where Just mTuple     = parseMessage ls
-             (message, rest) = mTuple
+extractMessage ls cmd     = if containsMessage cmd then (Just msg, rest) else (Nothing, ls)
+       where Just mTuple  = parseMessage ls
+             (msg, rest)  = mTuple
